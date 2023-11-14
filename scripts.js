2e29b9fb29a92e9830062a7c1daa78a2e6c007e2
AndrewDiGeronimo <andrewjdigeronimo@gmail.com>
2023-11-14 18:41:19 -0500

    Light and Dark themes added

--- scripts.js
+++ scripts.js
@@ -1,9 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.querySelector('.hamburger-menu');
     const navbar = document.querySelector('.navbar');
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const darkModeIcon = document.getElementById('darkModeIcon');
+
+    // Set initial theme based on system preference or saved preference
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const savedTheme = localStorage.getItem('theme');
+    const theme = savedTheme ? savedTheme : (prefersDarkMode ? 'dark' : 'light');
+    setTheme(theme);
 
     hamburger.addEventListener('click', function () {
         hamburger.classList.toggle('active');
         navbar.classList.toggle('active');
     });
+
+    darkModeToggle.addEventListener('click', function () {
+        const currentTheme = document.body.getAttribute('data-theme');
+        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        setTheme(newTheme);
+        localStorage.setItem('theme', newTheme);
+    });
+
+    function setTheme(theme) {
+        document.body.setAttribute('data-theme', theme);
+        darkModeIcon.className = theme === 'dark' ? 'bi bi-sun' : 'bi bi-moon';
+    }
 });
