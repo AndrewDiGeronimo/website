50fdad2e99897fa47a1b230783972c3646f657e9
AndrewDiGeronimo <andrewjdigeronimo@gmail.com>
2023-11-15 23:41:39 -0500

    Dark mode dynamic position adjustments

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     const hamburger = document.querySelector('.hamburger-menu');
     const navbar = document.querySelector('.navbar');
+
     const darkModeToggle = document.getElementById('darkModeToggle');
     const darkModeIcon = document.getElementById('darkModeIcon');
+
+    const footer = document.querySelector('footer');
 
     // Set initial theme based on system preference or saved preference
     const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -10,11 +13,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const theme = savedTheme ? savedTheme : (prefersDarkMode ? 'dark' : 'light');
     setTheme(theme);
 
+    //Hamburger Menu
     hamburger.addEventListener('click', function () {
         hamburger.classList.toggle('active');
         navbar.classList.toggle('active');
     });
 
+    //Dark Mode Toggle
     darkModeToggle.addEventListener('click', function () {
         const currentTheme = document.body.getAttribute('data-theme');
         const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
@@ -26,4 +31,23 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.setAttribute('data-theme', theme);
         darkModeIcon.className = theme === 'dark' ? 'bi bi-sun' : 'bi bi-moon';
     }
+
+    //Dynamically adjust darkmode button by position
+    function adjustDarkModeButton() {
+        const footerRect = footer.getBoundingClientRect();
+        const fromBottom = window.innerHeight - footerRect.top;
+        const footerVisible = fromBottom > 20 && fromBottom < window.innerHeight;
+
+        if (footerVisible) {
+            // Move button above the footer
+            const newPositionAboveFooter = window.innerHeight - footerRect.top + 10;
+            darkModeToggle.style.bottom = `${newPositionAboveFooter}px`;
+        } else {
+            // Keep button at its original fixed position
+            darkModeToggle.style.bottom = '30px';
+        }
+    }
+
+    window.addEventListener('scroll', adjustDarkModeButton);
+    adjustDarkModeButton(); // Adjust button position on load
 });
